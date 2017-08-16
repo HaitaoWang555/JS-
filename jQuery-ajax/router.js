507fb384a623c75136e024df7493c70c2e03d5af
王海涛 <15124505701@163.com>
2017-08-16 20:21:58 +0800

    addfile

---
+++ jQuery-ajax/router.js
@@ -0,0 +1,16 @@
+//  router.js
+
+
+app.get('/load-more', function(req, res) {
+
+  var curIdx = req.query.index
+  var len = req.query.length
+  var data = []
+
+  for(var i = 0; i < len; i++) {
+    data.push('新闻' + (parseInt(curIdx) + i))
+  }
+  setTimeout(function(){
+    res.send(data);
+   },1000)  // 模仿网速慢
+});
