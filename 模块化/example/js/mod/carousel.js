d7fc2322c3ef943ca547f21e06fbe2722ca68053
王海涛 <15124505701@163.com>
2018-03-23 16:49:00 +0800

    模块化

--- 模块化/example/js/mod/carousel.js
+++ 模块化/example/js/mod/carousel.js
@@ -1,44 +1,66 @@
 
-var $imgCt = $('#carousel')
-var $imgs = $('#carousel > li')
-var $bullet = $('#bullet')
-var $bullets = $('#bullet li')
+//模块化
+var Carousel = (function(){
 
-var $imgCount = $imgs.length
-var imgWidth = $imgs.width()
-
-var index
-var pageIndex = 0
-console.log(pageIndex)
-$imgCt.append($imgs.first().clone())
-$imgCt.prepend($imgs.last().clone())
-$imgCt.css({marginLeft: -imgWidth})
-
-function goNext(){
-  $imgCt.css({marginLeft: -imgWidth*(pageIndex+1)})
-  setBullet(pageIndex)
-  pageIndex += 1
-  if(pageIndex > $imgCount){
-    pageIndex = 1
-    $imgCt.css({marginLeft: -imgWidth})
-    setBullet(0)
+  var pageIndex = 0
+  var $imgCt,$imgs,$bullet,$bullets,$imgCount,imgWidth
+  var initJqVar = function(){
+    $imgCt = $('#carousel')
+    $imgs = $('#carousel > li')
+    $bullet = $('#bullet')
+    $bullets = $('#bullet li')
+    $imgCount = $imgs.length
+    imgWidth = $imgs.width()
   }
-}
 
-function setBullet(pageIndex){
-  $bullets.removeClass('active').eq(pageIndex).addClass("active")
-}
+  var initImgs = function(){
+    $imgCt.append($imgs.first().clone())
+    $imgCt.prepend($imgs.last().clone())
+    $imgCt.css({marginLeft: -imgWidth})
+  }
 
-function autoPlay() {
-  clock = setInterval(function () {
-    goNext()
-  }, 2000)
-}
+  var goNext = function(){
+    $imgCt.css({marginLeft: -imgWidth*(pageIndex+1)})
+    setBullet(pageIndex)
+    pageIndex += 1
+    if(pageIndex > $imgCount){
+      pageIndex = 1
+      $imgCt.css({marginLeft: -imgWidth})
+      setBullet(0)
+    }
+  }
 
-autoPlay()
-$bullets.on("click",function(){
-  clearInterval(clock)
-  pageIndex = $(this).index()
-  goNext()
-  autoPlay()
+  var setBullet = function(pageIndex){
+    $bullets.removeClass('active').eq(pageIndex).addClass("active")
+  }
+
+  var autoPlay = function(){
+    clock = setInterval(function () {
+      goNext()
+    }, 2000)
+  }
+
+  var listenBulletClick = function(){
+    $bullets.on("click",function(){
+      clearInterval(clock)
+      pageIndex = $(this).index()
+      goNext()
+      autoPlay()
+    })
+  }
+
+  return {
+    initCarousel: function(){
+      initJqVar();
+      initImgs();
+      autoPlay();
+      listenBulletClick();
+    }
+  }
+
+})()
+
+$(function(){
+  Carousel.initCarousel()
 })
+
