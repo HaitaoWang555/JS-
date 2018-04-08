0b5622b7a74330b7d022fd78677677da0288df8f
王海涛 <15124505701@163.com>
2018-04-08 16:13:13 +0800

    压缩合并js

--- gulpDemo/gulpfile.js
+++ gulpDemo/gulpfile.js
@@ -1,14 +1,25 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
 
-var cssnano = require('gulp-cssnano');
-var concat = require('gulp-concat');
+var cssnano = require("gulp-cssnano");
+var concat = require("gulp-concat");
+var uglify = require('gulp-uglify');
 
-gulp.task('build:css', function() {
-
-    gulp.src('./src/css/*.css')
-    		.pipe(concat('index-merge.css'))
-        .pipe(cssnano())
-        .pipe(gulp.dest('dist/css/'));
+gulp.task("css", function() {
+  gulp
+    .src("./src/css/*.css")
+    .pipe(concat("index-merge.css"))
+    .pipe(cssnano())
+    .pipe(gulp.dest("dist/css/"));
 });
 
-gulp.task('default', ['build:css'])
+
+
+gulp.task("js", function() {
+    gulp
+      .src("./src/js/*.js")
+      .pipe(concat("index-merge.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest("dist/js/"));
+  });
+  
+  gulp.task('build', ['css', 'js']);
