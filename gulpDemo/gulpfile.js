ec62c262f204820b0a8120c15e4e7c3612134c0d
王海涛 <15124505701@163.com>
2018-04-08 15:45:55 +0800

    压缩css

--- gulpDemo/gulpfile.js
+++ gulpDemo/gulpfile.js
@@ -1,1 +1,14 @@
 var gulp = require('gulp');
+
+var cssnano = require('gulp-cssnano');
+var concat = require('gulp-concat');
+
+gulp.task('build:css', function() {
+
+    gulp.src('./src/css/*.css')
+    		.pipe(concat('index-merge.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('default', ['build:css'])
