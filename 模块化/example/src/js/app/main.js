fb04a3186acd5f91c03dd030bc5068739550e4b7
wanghaitao <15124505701@163.com>
2018-04-12 11:05:16 +0800

    sass压缩引入

--- 模块化/example/src/js/app/main.js
+++ 模块化/example/src/js/app/main.js
@@ -1,21 +1,23 @@
+import './../../scss/app.scss';
 import $ from 'jquery';
 import Waterfall  from './../mod/waterFall.js';
 import Gotop  from './../mod/goTop.js';
 import Carousel  from './../mod/carousel.js';
-        getImgsBanner();
-        Waterfall.initWaterfall($('.picture_wrap'))
-        Gotop.init($('html'))
 
-      
-      function getImgsBanner() {
-        $.get(
-          "https://www.easy-mock.com/mock/5a70237883366960492d2bfb/get-images/banner"
-        )
-          .then(function(data) {
-            var C1 = $(".carousel_full");
-            Carousel.initCarousel(C1,data.imgLinks);
-          })
-      }
+  getImgsBanner();
+  Waterfall.initWaterfall($('.picture_wrap'))
+  Gotop.init($('html'))
+
+
+function getImgsBanner() {
+  $.get(
+    "https://www.easy-mock.com/mock/5a70237883366960492d2bfb/get-images/banner"
+  )
+    .then(function(data) {
+      var C1 = $(".carousel_full");
+      Carousel.initCarousel(C1,data.imgLinks);
+    })
+}
 
 
 
--- 模块化/example/webpack.config.js
+++ 模块化/example/webpack.config.js
@@ -1,24 +1,37 @@
 const path = require('path');
 webpack = require("webpack"),
 
-module.exports = {
-    entry: './src/js/app/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool:'source-map',
-    plugins:[
-        new webpack.ProvidePlugin({
-            $: 'jquery'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js[x]?$/,
-                exclude: /node_modules/
-            }
-        ]
-    }
-}
+	module.exports = {
+		entry: './src/js/app/main.js',
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, 'dist')
+		},
+		devtool: 'source-map',
+		plugins: [
+			new webpack.ProvidePlugin({
+				$: 'jquery'
+			})
+		],
+		module: {
+			rules: [
+				{
+					test: /\.js[x]?$/,
+					exclude: /node_modules/
+				},
+				{
+					test: /\.scss$/,
+					use: [{
+						loader: "style-loader"
+					}, {
+						loader: "css-loader"
+					}, {
+						loader: "sass-loader",
+						options: {
+							includePaths: ["absolute/path/a", "absolute/path/b"]
+						}
+					}]
+				}
+			]
+		}
+	}
